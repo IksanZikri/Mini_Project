ae0b1ae0cb4005396fa5b0eadc89c2c8cbc42a3d
root <root@localhost.localdomain>
2025-05-26 19:50:51 +0700

    Perubahan app.js V2

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello from Node.js App! V1'));
+app.get('/', (req, res) => res.send('Hello from Node.js App! V2'));
 app.listen(port, () => console.log(`App listening on port ${port}`));
 
